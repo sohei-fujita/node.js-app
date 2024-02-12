60b4ce7919facf3adcb167a1d9b4cbb813c08c43
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-02-12 19:14:08 +0900

    event pool

---
+++ sample.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+  console.log("タイマー関数が呼ばれました")
+}, 3000);
+
+setTimeout(function(){
+  console.log("タイマー関数が呼ばれました")
+}, 3000); 
+
+setTimeout(function(){
+  console.log("タイマー関数が呼ばれました")
+}, 3000); 
