a6e9b74c4dee8c2928f179ad7880a59b0a7604c5
sohei-fujita <nnn.sohei.fujita@gmail.com>
2024-02-14 19:53:24 +0900

    method function

--- sample.js
+++ sample.js
@@ -1,4 +1,22 @@
-const {name, add} = require("./helper");
+const fs = require('fs')
 
-console.log(add(2, 3));
-console.log(name)
+// fs.writeFile("sample.txt", "hello world", function(){
+//   console.log("書き出し完了！")
+// })
+
+console.log("end")
+
+console.log(process.argv[2])
+
+const request = process.argv[2];
+if(request === "read"){
+  fs.readFile("./sample.txt", "utf8", function (err, data) {
+    console.log(data)
+  });  
+} else if(request === "write"){
+  fs.writeFile("sample.txt", "hello world!!!?", function(){
+    console.log("書き出し完了！")
+  })
+} else {
+  console.error("エラー")
+}
